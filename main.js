9eb22f6a2367126f34261f16bb698b5b0e0e9f9e
djonesdev <31727436+djonesdev@users.noreply.github.com>
2017-09-18 15:47:29 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
  $(function() {
 
-var score = 0;
-
-   var button = $('#box')
-   var elem = $('#message');
-   var count = 0;
-   var gunshot = $('#gunshot');
+	var score = 0;
+	var button = $('#box')
+	var elem = $('#message');
+	var count = 0;
+	var gunshot = $('#gunshot');
 
  // INSTRUCTION PANEL 
 
@@ -33,66 +32,87 @@ var score = 0;
 
 // SHOT FUNCTIONS
 
-$('#gameStage').on('click', function (event) {
+	$('#gameStage').on('click', function (event) {
         var audio = new Audio('audio/shotgun-mossberg590-RA_The_Sun_God-451502290.mp3');
 
         audio.play();    
     });
 
-$(function() {
-	var count = 0;
-	$('#target').click(function(e) {
-		count++;
-			$('#gunshot').show();
+	$(function() {
+		var count = 0;
+		$('#target').click(function(e) {
+			count++;
+			checkBossActivation()
+				$('#gunshot').show();
+				score += 10;
+				$('#score').html(score);
+				setTimeout(function() {
+					$('#gunshot').hide();
+				}, 3500);
+		});
+
+	});
+
+	$(function() {
+		$('#target2').click(function shotZombie2(e) {
 			score += 10;
-			$('#score').text(score);
-			setTimeout(function() {
-				$('#gunshot').hide();
-			}, 3500);
+			checkBossActivation()
+				$('#score').text(score);
+				$('#gunshot2').show();
+				setTimeout(function() {
+					$('#gunshot2').hide();
+				},2500);
+		});
 	});
 
-});
+	$(function() {
+		$('#target3').click(function(e) {
+			score += 10;
+			checkBossActivation();
+				$('#score').text(score);
+				$('#gunshot3').show();
+				setTimeout(function() {
+					$('#gunshot3').hide();
+				}, 2500);
 
-$(function() {
-	$('#target2').click(function shotZombie2(e) {
-		score += 10;
-			$('#score').text(score);
-			$('#gunshot2').show();
-			setTimeout(function() {
-				$('#gunshot2').hide();
-			},2500);
+		});
 	});
-});
 
-$(function() {
-	$('#target3').click(function(e) {
+	$('#target4').click(function (e) {
 		score += 10;
+		checkBossActivation();
 			$('#score').text(score);
-			$('#gunshot3').show();
-			setTimeout(function() {
-				$('#gunshot3').hide();
+			$('#gunshot4').show();
+			setTimeout(function (e) {
+				$('#gunshot4').hide();
 			}, 2500);
-
 	});
-});
 
-$('#target4').click(function (e) {
-	score += 10;
-		$('#score').text(score);
-		$('#gunshot4').show();
-		setTimeout(function (e) {
-			$('#gunshot4').hide();
-		}, 2500);
-});
+	$('#target5').click(function (e) {
+		score += 10;
+		checkBossActivation();
+			$('#score').text(score);
+			$('#gunshot5').show();
+			setTimeout(function (e) {
+				$('#gunshot5').hide();
+			}, 3000);
+	});
 
-$('#target5').click(function (e) {
-	score += 10;
-		$('#score').text(score);
-		$('#gunshot5').show();
-		setTimeout(function (e) {
-			$('#gunshot5').hide();
-		}, 3000);
-});
+	$('#bossTarget').click(function (e) {
+		$('#gunshot6').show();
+		count++;
+		if (count === 10) {
+			score += 1000;
+			$('#score').text(score);
+			$('#bossTarget').fadeOut();
+		};
+	});
+
+	function checkBossActivation () {
+	 if ($('#score').html() === '200') {
+	 	$('#bossTarget').show()
+	 };
+	}
 
 // ANIMATION LOOPS
 
@@ -114,42 +134,46 @@ $('#target5').click(function (e) {
 });
 
 
-setInterval(function (e) {
-	$('#target').animate({
-		bottom: '20%'
-	}, 4000)
-		.animate({
-			bottom: '0%'
-		});
-}, 7000);
+	setInterval(function (e) {
+		$('#target').animate({
+			bottom: '20%'
+		}, 4000)
+			.animate({
+				bottom: '0%'
+			});
+	}, 7000);
 
- $(function animateLoop4(e) {
- 	$('#target4').animate({
- 		bottom: '20%'
- 	}, 2000)
- 	.animate({
- 		right: '20%',
- 	}, 2000);
- 	$('#target4').fadeOut().delay(2000);
- 	$('#target4').animate({
- 		right: '5%'
- 	})
- 	$('#target4').fadeIn().delay(2000)
- 		$(function () {
- 			animateLoop4();
- 		});
+	$(function animateLoop4(e) {
+	 	$('#target4').animate({
+	 		bottom: '20%'
+	 	}, 500)
+	 	.animate({
+	 		right: '20%',
+	 	}, 2000);
+	 	$('#target4').fadeOut().delay(1000);
+	 	$('#target4').animate({
+	 		right: '5%'
+	 	});
+	 	$('#target4').fadeIn().delay(500)
+	 		$(function () {
+	 			animateLoop4();
+	 		});
 
- }); 
+	}); 
 
- $(function animateLoop3(e) {
- 	$('#target3').delay(8000).fadeIn();
- 	$('#target3').delay(2500).fadeOut();
- });
+	setInterval(function animateLoop3(e) {
+	 	$('#target3').delay(3000).fadeIn();
+		$('#target3').delay(2500).fadeOut();
+	}, 3000);
 
- setInterval(function (e) {
- 	$('#target5').delay(2000).fadeOut()
- 	$('#target5').delay(2000).slideDown();
- }, 2000);
+	setInterval(function (e) {
+		$('#target5').delay(2000).fadeOut()
+		$('#target5').delay(2000).slideDown();
+	}, 2000);
+
+	 // if ($('#score').html() === '200') {
+	 // 	$('#bossTarget').show()
+	 // };
 });
 });
 // });
